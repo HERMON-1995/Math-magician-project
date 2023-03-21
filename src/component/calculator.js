b3752409653560e1fa24773bc34c4931bf529002
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-21 15:32:35 +0300

    Add one child component in calculator.js

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -1,41 +1,126 @@
-/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-class Calculator extends React.Component {
-  render() {
-    return (
-      <div className="calculator">
-        <div className="display-section">
-          <p className="diplay-value">0</p>
-        </div>
-
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="operator">÷</button>
-
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="operator">x</button>
-
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="operator">-</button>
-
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="operator">+</button>
-
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="operator">=</button>
-
-      </div>
-    );
-  }
+function Result({ result }) {
+  return (
+    <div className="display-section">
+      <p className="diplay-value">{result}</p>
+    </div>
+  );
 }
+
+function Calculator() {
+  return (
+    <div className="calculator">
+      <Result result={0} />
+
+      <button type="button">AC</button>
+      <button type="button">+/-</button>
+      <button type="button">%</button>
+      <button type="button" className="operator">÷</button>
+
+      <button type="button">7</button>
+      <button type="button">8</button>
+      <button type="button">9</button>
+      <button type="button" className="operator">x</button>
+
+      <button type="button">4</button>
+      <button type="button">5</button>
+      <button type="button">6</button>
+      <button type="button" className="operator">-</button>
+
+      <button type="button">1</button>
+      <button type="button">2</button>
+      <button type="button">3</button>
+      <button type="button" className="operator">+</button>
+
+      <button type="button" className="zero">0</button>
+      <button type="button">.</button>
+      <button type="button" className="operator">=</button>
+    </div>
+  );
+}
+
+// class Result extends React.Component {
+//   render() {
+//     return (
+//       <div className="display-section">
+//         <p className="diplay-value">{this.props.result}</p>
+//       </div>
+//     );
+//   }
+// }
+
+// class Calculator extends React.Component {
+//   render() {
+//     return (
+//       <div className="calculator">
+//         <Result result={0} />
+
+//         <button type="button">AC</button>
+//         <button type="button">+/-</button>
+//         <button type="button">%</button>
+//         <button type="button" className="operator">÷</button>
+
+//         <button type="button">7</button>
+//         <button type="button">8</button>
+//         <button type="button">9</button>
+//         <button type="button" className="operator">x</button>
+
+//         <button type="button">4</button>
+//         <button type="button">5</button>
+//         <button type="button">6</button>
+//         <button type="button" className="operator">-</button>
+
+//         <button type="button">1</button>
+//         <button type="button">2</button>
+//         <button type="button">3</button>
+//         <button type="button" className="operator">+</button>
+
+//         <button type="button" className="zero">0</button>
+//         <button type="button">.</button>
+//         <button type="button" className="operator">=</button>
+
+//       </div>
+//     );
+//   }
+// }
+
+// class Calculator extends React.Component {
+//   render() {
+//     return (
+//       <div className="calculator">
+//         <div className="display-section">
+//           <p className="diplay-value">0</p>
+//         </div>
+
+//         <button type="button">AC</button>
+//         <button type="button">+/-</button>
+//         <button type="button">%</button>
+//         <button type="button" className="operator">÷</button>
+
+//         <button type="button">7</button>
+//         <button type="button">8</button>
+//         <button type="button">9</button>
+//         <button type="button" className="operator">x</button>
+
+//         <button type="button">4</button>
+//         <button type="button">5</button>
+//         <button type="button">6</button>
+//         <button type="button" className="operator">-</button>
+
+//         <button type="button">1</button>
+//         <button type="button">2</button>
+//         <button type="button">3</button>
+//         <button type="button" className="operator">+</button>
+
+//         <button type="button" className="zero">0</button>
+//         <button type="button">.</button>
+//         <button type="button" className="operator">=</button>
+
+//       </div>
+//     );
+//   }
+// }
 
 export default Calculator;
