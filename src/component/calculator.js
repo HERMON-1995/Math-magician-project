e8b7dcaa63484e7bb3c49e7acfa0b8f7b727b03d
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-21 14:16:10 +0300

    Update calculator.js

--- src/component/calculator.js
+++ src/component/calculator.js
@@ -2,40 +2,40 @@
 import React from 'react';
 
 class Calculator extends React.Component {
-    render(){
-       return (
-         <div className="calculator">
-            <div className="display-section">
-               <p className='diplay-value'>0</p>
-            </div>
+  render() {
+    return (
+      <div className="calculator">
+        <div className="display-section">
+          <p className="diplay-value">0</p>
+        </div>
 
-            <button type="button">AC</button>
-            <button type="button">+/-</button>
-            <button type="button">%</button>
-            <button type="button" className="operator">÷</button>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="operator">÷</button>
 
-            <button type="button">7</button>
-            <button type="button">8</button>
-            <button type="button">9</button>
-            <button type="button" className="operator">x</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="operator">x</button>
 
-            <button type="button">4</button>
-            <button type="button">5</button>
-            <button type="button">6</button>
-            <button type="button" className="operator">-</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="operator">-</button>
 
-            <button type="button">1</button>
-            <button type="button">2</button>
-            <button type="button">3</button>
-            <button type="button" className="operator">+</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="operator">+</button>
 
-            <button type="button" className="zero">0</button>
-            <button type="button">.</button>
-            <button type="button" className="operator">=</button>
+        <button type="button" className="zero">0</button>
+        <button type="button">.</button>
+        <button type="button" className="operator">=</button>
 
-         </div>
-       );
-    }
+      </div>
+    );
+  }
 }
 
 export default Calculator;
