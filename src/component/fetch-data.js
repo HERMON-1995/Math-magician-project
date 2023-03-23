d7e37440c2cdbeba4f325de2f998f0442276b300
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-23 15:41:26 +0300

    Add component to fetch data from the API

---
+++ src/component/fetch-data.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
+function Quote() {
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const category = 'computers';
+    const quotesUrl = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+    const apiKey = 'taCIDPxMA81DWfKUu0b2JZGQDpHPfH0R4BNcEs0K';
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(quotesUrl, {
+          headers: {
+            'Content-Type': 'application/json',
+            'X-Api-Key': apiKey,
+          },
+        });
+
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setHasError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, [setIsLoading, setHasError]);
+
+  if (hasError) return <div>Something went wrong!</div>;
+
+  if (isLoading) return <div>Loading...</div>;
+  const index = Date.now;
+  return (
+    <div>
+      {data.map((quotes) => (
+        <div key={index} className="quote-div">
+          &quot;
+          {quotes.quote}
+          &quot;
+          <br />
+          ➖
+          <strong className="author">{quotes.author}</strong>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Quote;
