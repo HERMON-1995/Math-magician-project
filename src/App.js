7b9df0c09731c537c8a64b5ebe0a9bead792e942
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-23 15:40:44 +0300

    Add div to render quote in the browser

--- src/App.js
+++ src/App.js
@@ -2,10 +2,25 @@
 import './App.css';
 import React from 'react';
 import Calculator from './component/calculator';
+import Quote from './component/fetch-data';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
-    return (<Calculator />);
+    return (
+      <div className="allContent">
+        <div className="quote">
+          <Quote />
+        </div>
+        <div className="calculator-wrap">
+          <Calculator />
+        </div>
+      </div>
+    );
   }
 }
 
