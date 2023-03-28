db37c5bcb10fe74158f21de71d078aba805a441e
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-28 19:05:39 +0300

    Add useMemo and useCallback from react

--- src/component/fetch-data.js
+++ src/component/fetch-data.js
@@ -1,53 +1,63 @@
-import React, { useState, useEffect } from 'react';
+import React, {
+  useState, useEffect, useMemo, useCallback,
+} from 'react';
 
 function Quote() {
   const [data, setData] = useState([]);
   const [hasError, setHasError] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
+  const category = 'computers';
+  const quotesUrl = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
+  const apiKey = 'taCIDPxMA81DWfKUu0b2JZGQDpHPfH0R4BNcEs0K';
+
+  const fetchData = useCallback(async () => {
+    setIsLoading(true);
+    try {
+      const res = await fetch(quotesUrl, {
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Api-Key': apiKey,
+        },
+      });
+
+      const json = await res.json();
+      setData(json);
+    } catch (error) {
+      setHasError(true);
+    }
+    setIsLoading(false);
+  }, [quotesUrl, apiKey]);
+
   useEffect(() => {
-    const category = 'computers';
-    const quotesUrl = `https://api.api-ninjas.com/v1/quotes?category=${category}`;
-    const apiKey = 'taCIDPxMA81DWfKUu0b2JZGQDpHPfH0R4BNcEs0K';
-
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const res = await fetch(quotesUrl, {
-          headers: {
-            'Content-Type': 'application/json',
-            'X-Api-Key': apiKey,
-          },
-        });
-
-        const json = await res.json();
-        setData(json);
-      } catch (error) {
-        setHasError(true);
-      }
-      setIsLoading(false);
+    let mounted = true;
+    if (mounted) {
+      fetchData();
+    }
+    return () => {
+      mounted = false;
     };
-    fetchData();
-  }, [setData, setIsLoading]);
+  }, [fetchData]);
+
+  const quotes = useMemo(() => data, [data]);
 
   if (hasError) return <div className="status"><strong>Something went wrong!</strong></div>;
 
   if (isLoading) return <div className="status"><strong>Loading...</strong></div>;
-  const index = Date.now;
+
   return (
     <div>
-      {data.map((quotes) => (
-        <div key={index} className="quote-div">
+      {quotes.map((quote) => (
+        <div key={quote.id} className="quote-div">
           &quot;
-          {quotes.quote}
+          {quote.quote}
           &quot;
           <br />
           ➖
-          <strong className="author">{quotes.author}</strong>
+          <strong className="author">{quote.author}</strong>
         </div>
       ))}
     </div>
   );
 }
-
 export default Quote;
