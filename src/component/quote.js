aa8da0b5b9cfc9557273ffedf6189015aa86caf7
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-28 19:06:18 +0300

    Add quote page

---
+++ src/component/quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Quote from './fetch-data';
+
+const QuotePage = () => (
+  <div className="quote-container">
+    <Quote />
+  </div>
+);
+export default QuotePage;
