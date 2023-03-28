cfcc1f836cda367e62fa9674b9f5ba752f9089aa
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-28 19:04:39 +0300

    Add nav links

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-/* eslint-disable react/prefer-stateless-function */
 import './App.css';
-import React from 'react';
+import { Route, Routes, NavLink } from 'react-router-dom';
+import HomePage from './component/home';
+import QuotePage from './component/quote';
 import Calculator from './component/calculator';
-import Quote from './component/fetch-data';
 
-function App() {
-  return (
-    <div>
-      <div>
-        <Quote />
-      </div>
-      <div>
-        <Calculator />
-      </div>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <nav>
+      <h1>Math Magicians</h1>
+      <ul>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/calculator">Calculator</NavLink>
+        <NavLink to="/quote">Quote</NavLink>
+      </ul>
+    </nav>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<QuotePage />} />
+    </Routes>
+  </>
+);
 
 export default App;
