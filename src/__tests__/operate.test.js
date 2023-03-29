b72b73da43e3e93ff48449b2e290abcf9dbeabf3
HERMON-1995 <hermonghiwot1995@gmail.com>
2023-03-29 17:09:11 +0300

    Create operate.test.js

---
+++ src/__tests__/operate.test.js
@@ -0,0 +1,28 @@
+import operate from '../logic/operate';
+
+describe('operations in calculator', () => {
+    test('multiply 3*3 to equal 9', () => {
+      const result = operate(3, 3, 'x');
+      expect(result).toBe('9');
+    });
+    test('division of 10/2', () => {
+      const divide = operate(10, 2, '÷');
+      expect(divide).toBe('5');
+    });
+    test('substraction to substract 10-2', () => {
+      const substract = operate(2, 3, '-');
+      expect(substract).toBe('-1');
+    });
+    test('addition of 10+2', () => {
+      const addition = operate(10, 2, '+');
+      expect(addition).toBe('12');
+    });
+    test('modulos', () => {
+      const modulos = operate(5, 2, '%');
+      expect(modulos).toBe('1');
+    });
+    test('multiply 10*2', () => {
+      const result = operate(10, 2, 'x');
+      expect(result).toBe('20');
+    });
+  });
